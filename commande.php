e4d83bfa5c76018095d3af5d2e5069d8173c4af2
mohamed-azzouz <mohamed.azzouz@laplateforme.io>
2020-07-28 16:29:50 +0200

    poush

--- commande.php
+++ commande.php
@@ -75,10 +75,37 @@
 						}
 					}
 					?>
+					<div id="paypal-button-container"></div>
+					<script src="https://www.paypal.com/sdk/js?client-id=sb&currency=EUR" data-sdk-integration-source="button-factory"></script>
+					<script>
+						paypal.Buttons({
+							style: {
+								shape: 'pill',
+								color: 'gold',
+								layout: 'vertical',
+								label: 'paypal',
+								
+							},
+							createOrder: function(data, actions) {
+								return actions.order.create({
+									purchase_units: [{
+										amount: {
+											value: '10'
+										}
+									}]
+								});
+							},
+							onApprove: function(data, actions) {
+								return actions.order.capture().then(function(details) {
+									alert('Transaction completed by ' + details.payer.name.given_name + '!');
+								});
+							}
+						}).render('#paypal-button-container');
+					</script>
 				</tbody>
 			</table>
-		
-	</main>
+			
+		</main>
 
 </body>
 </html>
